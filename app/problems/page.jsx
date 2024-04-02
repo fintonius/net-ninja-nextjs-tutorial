894ff310f33ff541900dde588c7372334b5059dc
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-04-02 12:53:49 +0100

    first commit

---
+++ app/problems/page.jsx
@@ -0,0 +1,9 @@
+import RenderFromTemplateContext from "next/dist/client/components/render-from-template-context"
+
+export default function Tickets() {
+    return (
+        <main>
+            <h2>Problems</h2>
+        </main>
+    )
+}
