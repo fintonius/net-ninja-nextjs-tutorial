17f27688b0bb4d3554f839e4c459f63a8f17e0f3
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-04-06 18:41:59 +0100

    connect to the local json server and present the data to the user

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Dogo from './fune.png'
+
+export default function Navbar() {
+    return (
+    <nav>
+        <Image 
+          src={Dogo}
+          alt="It is fune"
+          width={70}
+          quality={100}
+          placeholder='blur'
+        />
+        <h1>Fintan's Many Problems</h1>
+        <Link href="/">Dashboard </Link>
+        <Link href="/tickets">Problems</Link>
+      </nav>
+    )
+}
---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,41 @@
+async function getTickets() {
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            /* using 0 opts out of Next caching the data from the initial fetch request
+            if you add another value that is the amount of seconds Next will wait before
+            making another fetch request to get the latest data. 
+            
+            However, it does cache the data and will serve this to a user first even if the 
+            data has been updated, as that is what is quicker. Next will update the cache
+            with this newly fetched data and the next time a user visits the page this new
+            data will be served. Seems not the most efficient tbh!
+            */
+            revalidate: 0
+        }
+    })
+    
+    return res.json()
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets();
+    console.log(tickets)
+
+    return (
+        <>
+           {tickets.map((ticket) => (           
+                <div key={ticket.id} >
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className={`pill ${ticket.priority}`}>
+                        {ticket.priority} priority
+                    </div>
+                </div>
+            ))}
+            {tickets.length === 0 && (
+                <p className="text-center">There are no open tickets</p>
+            )}
+            
+        </>
+    )
+  }
