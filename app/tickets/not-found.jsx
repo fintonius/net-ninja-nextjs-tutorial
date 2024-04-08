711892c350c1fc88fab0e677d2bc2d0805928d29
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-04-08 16:21:56 +0100

    adding custom 404 error page

---
+++ app/tickets/not-found.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <main className="text-center">
+            <h2 className="text-3xl">We hit a brick wall</h2>
+            <p>We couldn't find the ticket you wanted.</p>
+            <p>Go back to the <Link href="/tickets">Tickets</Link> page.</p>
+        </main>
+    )
+}
